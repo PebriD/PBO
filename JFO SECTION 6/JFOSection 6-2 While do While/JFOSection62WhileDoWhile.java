6a7b44967903a183fd1197ee077023354adf8c90
Pebri Darmawan <zhenghanggigamming@gmail.com>
2022-06-03 12:56:09 +0700

    Pertemuan 06

---
+++ JFO SECTION 6/JFOSection 6-2 While do While/JFOSection62WhileDoWhile.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package jfosection.pkg6.pkg2.pkgwhile.pkgdo.pkgwhile;
+
+/**
+ *
+ * @author USER
+ */
+public class JFOSection62WhileDoWhile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int y=1;
+do{
+System.out.println(y+" ");
+y++;
+}while (y<=20);
+    
+   
+ }
+}
