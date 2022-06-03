d201b5a05831c41488226eb8093c25e787ee7486
Pebri Darmawan <zhenghanggigamming@gmail.com>
2022-06-03 13:01:16 +0700

    Pertemuan 03

---
+++ pertemuan 03/src/Ular.java
@@ -0,0 +1,25 @@
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class Ular {
+  
+        add(new Arena());    
+        Ular(false);
+        Ular();
+        setTitle("ular");
+        ImageIcon icon = new ImageIcon("src/ular.png");
+        setIconImage(icon.getImage());
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
