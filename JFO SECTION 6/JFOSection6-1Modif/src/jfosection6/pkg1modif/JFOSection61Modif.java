d4cd700d1b0234599b715a478d32e088bd11453f
Pebri Darmawan <zhenghanggigamming@gmail.com>
2022-06-03 12:55:54 +0700

    Pertemuan 06

---
+++ JFO SECTION 6/JFOSection6-1Modif/src/jfosection6/pkg1modif/JFOSection61Modif.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package jfosection6.pkg1modif;
+
+/**
+ *
+ * @author USER
+ */
+public class JFOSection61Modif {
+
+    public static void main(String[] args) {
+       System.out.println("Looping: ");
+
+for (int i = 0; i <= 5; i=i+2){ 
+System.out.println( i + " ");
+}
+System.out.println("Blass Off!");
+    }
+    }
+    
+
---
+++ JFO SECTION 6/src/jfo/section/pkg6/looping/JFOSection6Looping.java
@@ -0,0 +1,22 @@
+
+package jfo.section.pkg6.looping;
+
+public class JFOSection6Looping {
+
+    private static String i;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+System.out.println("Looping: ");
+
+for (int i = 5; i >= 0; i--){ 
+System.out.println( i + " ");
+}
+System.out.println("Blass Off!");
+    }
+}
+
+    
